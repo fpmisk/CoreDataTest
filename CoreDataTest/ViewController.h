0035a47f75fce128a134e695569984a81220c250
fpmisk <admin@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2015-10-08 08:54:18 +0300

    Project init

---
+++ CoreDataTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataTest
+//
+//  Created by Admin on 07.10.15.
+//  Copyright (c) 2015 bsu.fpmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
